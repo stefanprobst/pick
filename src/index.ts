0e35999616531cfee61c0cea27f6349ac768f919
Stefan Probst <stefanprobst@posteo.de>
2021-10-26 20:41:35 +0200

    feat: add pick fn

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export function pick<T extends Record<string, unknown>, K extends keyof T>(
+  obj: T,
+  keys: Array<K>
+): Pick<T, K> {
+  const o = {} as Pick<T, K>;
+
+  keys.forEach((key) => {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      o[key] = obj[key];
+    }
+  });
+
+  return o;
+}
